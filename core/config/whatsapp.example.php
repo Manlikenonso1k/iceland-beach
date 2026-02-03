e6e0482b06f41ec8e3b94aedd7862edf0a973cbf
Manlikenonso1k <victorynonso9@gmail.com>
2026-02-03 16:42:50 +0100

    test push

--- core/config/whatsapp.example.php
+++ core/config/whatsapp.example.php
@@ -1,6 +1,6 @@
 <?php
 return [
     // Use country code + number (no +)
-    'callmebot_phone' => '2349036033056',
+    'callmebot_phone' => '23490360330506',
     'callmebot_key' => 'YOUR_API_KEY',
 ];
