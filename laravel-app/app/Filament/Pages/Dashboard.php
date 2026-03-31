ec919196b301415dfbaf6e5591b262a3117bec9c
Manlikenonso1k <victorynonso9@gmail.com>
2026-03-31 11:52:20 +0100

    added fillaemt dashboardd page

---
+++ laravel-app/app/Filament/Pages/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    // You can customize widgets and layout here if needed
+}
